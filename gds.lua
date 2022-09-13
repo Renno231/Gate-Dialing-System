6c10ef49e2996f879f7b1a98db311ce5ec1e75e7
Renno231 <meansofalternation@protonmail.com>
2022-09-13 02:55:12 -0500

    added more functionality for options

--- gds.lua
+++ gds.lua
@@ -2,18 +2,24 @@ local shell = require("shell")
 local filesystem = require("filesystem")
 
 local args, opts = shell.parse(...)
+local gdsFiles = {"/bin/gds.lua", "/gds/gatecomputer.lua","/lib/clientinterface.lua","/lib/guilist.lua","/lib/guiwindow.lua","/lib/guibutton.lua","/lib/guitextbox.lua"}
 
 local options = "-"
 for k,v in pairs(opts) do options = options..tostring(k) end
 if opts.u then --update
     local gdsType = (filesystem.exists("/gds/clientinterface.lua") and "-c") or (filesystem.exists("/gds/gatecomputer.lua") and "-g")
     if gdsType then
-        local downloadedInstaller = shell.execute("wget -f https://raw.githubusercontent.com/Renno231/Gate-Dialing-System/main/installer.lua")
-        if downloadedInstaller then
-            print("Downloaded installer...")
+        if filesystem.exist("/home/installer.lua") then
+            print("Running GDS installer...")
             shell.execute("/home/installer.lua "..gdsType)
         else
-            os.exit()
+            local downloadedInstaller = shell.execute("wget -f https://raw.githubusercontent.com/Renno231/Gate-Dialing-System/main/installer.lua")
+            if downloadedInstaller then
+                print("Downloaded installer...\nRunning GDS installer...")
+                shell.execute("/home/installer.lua "..gdsType)
+            else
+                os.exit()
+            end
         end
     else
         print("Installation not detected.")
@@ -24,9 +30,9 @@ elseif (opts.c or opts.g) then
         shell.execute("wget -f https://raw.githubusercontent.com/Renno231/Gate-Dialing-System/main/installer.lua")
     end
     shell.execute("/home/installer.lua "..options)
-elseif opts.i then --install AGS addresses
+elseif opts.i then --import AGS addresses
 
-elseif opts.d then --delete
+elseif opts.d then --delete, need to add detection for autorun in /home/.shrc
     for i, address in ipairs (gdsFiles) do
         if filesystem.exists(address) then
             filesystem.remove(address)
@@ -34,7 +40,31 @@ elseif opts.d then --delete
     end
 end
 if opts.a then --autostart
-
+    if filesystem.exists("/home/.shrc") then
+        local line, foundGDSAuto = nil, false
+        local file, reason = io.open("/home/.shrc")
+        repeat
+            local line = file["readLine"](file, false)
+            if line then
+                foundGDSAuto = line=="/bin/gds.lua"
+            end
+        until not line or foundGDSAuto
+        file:close()
+        if not foundGDSAuto then
+            file, reason = io.open("/home/.shrc", "a")
+            file:write("/bin/gds.lua")
+            file:close()
+            print("Created autorun for GDS.")
+        else
+            print("GDS is already autorun.")
+        end
+    else
+        local file, reason = io.open("/home/.shrc", "w")
+        file:write("/bin/gds.lua")
+        file:close()
+        print("Created autorun for GDS.")
+    end
+    print("To remove GDS from autorun, edit the /home/.shrc file and remove the line with /bin/gds.lua")
 end
 if filesystem.exists("/gds/clientinterface.lua") then
     shell.execute("/gds/clientinterface.lua")
