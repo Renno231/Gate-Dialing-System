e027479237e976b4cd5d68a8049438f467f96a8c
Renno231 <meansofalternation@protonmail.com>
2022-10-27 04:13:56 -0500

    added additional checks for autostart

--- gds.lua
+++ gds.lua
@@ -4,10 +4,10 @@ local filesystem = require("filesystem")
 local args, opts = shell.parse(...)
 local gdsFiles = {"/bin/gds.lua", "/gds/gatecomputer.lua","/lib/clientinterface.lua","/lib/guilist.lua","/lib/guiwindow.lua","/lib/guibutton.lua","/lib/guitextbox.lua"}
 
+local gdsType = (filesystem.exists("/gds/clientinterface.lua") and "-c") or (filesystem.exists("/gds/gatecomputer.lua") and "-g")
 local options = "-"
 for k,v in pairs(opts) do options = options..tostring(k) end
 if opts.u then --update
-    local gdsType = (filesystem.exists("/gds/clientinterface.lua") and "-c") or (filesystem.exists("/gds/gatecomputer.lua") and "-g")
     if gdsType then
         if filesystem.exists("/home/installer.lua") then
             print("Running GDS installer...")
@@ -37,7 +37,7 @@ elseif opts.d then --delete, need to add detection for autorun in /home/.shrc
         end
     end
 end
-if opts.a then --autostart
+if opts.a and gdsType then --autostart
     local autorunFile = [[function start(msg)
 local shell = require("shell") 
     do 
@@ -45,7 +45,7 @@ local shell = require("shell")
     end
 end]] 
     local file = io.open("/etc/rc.d/gds.lua", "w") 
-    file:write(autorunFile:format(filesystem.exists("/gds/clientinterface.lua") and "clientinterface" or "gatecomputer")) 
+    file:write(autorunFile:format(gdsType=="-c" and "clientinterface" or "gatecomputer")) 
     file:close()
     shell.execute("rc gds enable")
 end     
@@ -57,3 +57,4 @@ elseif filesystem.exists("/gds/gatecomputer.lua") then
 else
     io.stderr:write("GDS isn't installed.\n")
 end
+  
