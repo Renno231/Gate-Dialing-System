b5f915a87afdd6abc328e6e2c4ced940fe635e51
Renno231 <meansofalternation@protonmail.com>
2023-02-21 13:51:32 -0600

    improved uninstall option

--- gds.lua
+++ gds.lua
@@ -2,7 +2,7 @@ local shell = require("shell")
 local filesystem = require("filesystem")
 
 local args, opts = shell.parse(...)
-local gdsFiles = {"etc/rc.d/gds.lua","/bin/gds.lua", "/gds/gatecomputer.lua","/lib/clientinterface.lua","/lib/guilist.lua","/lib/guiwindow.lua","/lib/guibutton.lua","/lib/guitextbox.lua"}
+local gdsFiles = {"/bin/gds.lua", "/gds/gatecomputer.lua","/lib/clientinterface.lua","/lib/guilist.lua","/lib/guiwindow.lua","/lib/guibutton.lua","/lib/guitextbox.lua"}
 
 local gdsType = (filesystem.exists("/gds/clientinterface.lua") and "-c") or (filesystem.exists("/gds/gatecomputer.lua") and "-g")
 local options = "-"
@@ -34,10 +34,17 @@ elseif opts.d then --delete, need to add detection for autorun in /home/.shrc
     for i, address in ipairs (gdsFiles) do
         if filesystem.exists(address) then
             filesystem.remove(address)
+            print("removed",address)
         end
     end
+    if filesystem.exists("/etc/rc.d/gds.lua") then
+        filesystem.remove("/etc/rc.d/gds.lua")
+        shell.execute("rc gds disable")
+        print("removed autostart")
+    end
+    print("uninstalled GDS files.")
 end
-if opts.a and gdsType then --autostart
+if opts.a and gdsType then --autostart, include the shell.execute("install gds") to auto update GDS from floppy if its present
     local autorunFile = [[function start(msg)
 local shell = require("shell") 
     do 
@@ -50,11 +57,10 @@ end]]
     shell.execute("rc gds enable")
 end     
 
-if filesystem.exists("/gds/clientinterface.lua") then
-    shell.execute("/gds/clientinterface.lua")
-elseif filesystem.exists("/gds/gatecomputer.lua") then
-    shell.execute("/gds/gatecomputer.lua")
-else
-    io.stderr:write("GDS isn't installed.\n")
+if #args == 0 then
+    if gdsType then
+        shell.execute("/gds/"..(gdsType=="-c" and "clientinterface" or "gatecomputer")..".lua")
+    else
+        print("GDS isn't installed.\n")
+    end
 end
-  
